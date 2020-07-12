98f0d58c65c3a2721f4dfbf69cf42b1bc0a23621
Silas Jo <52027648+starmaster1126@users.noreply.github.com>
2020-07-12 13:38:30 -0400

    zother

--- Header.m
+++
@@ -1,9 +0,0 @@
-//
-//  Header.m
-//  Recipy
-//
-//  Created by eric on 2019/8/26.
-//  Copyright © 2019 Sebastian Jolly. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
--- README.md
+++
@@ -1,2 +0,0 @@
-# Recipy
-Small swift project using the food2fork API
